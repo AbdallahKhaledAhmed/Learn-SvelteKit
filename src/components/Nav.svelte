3ca5e5d6b1b3180a8dd3fb71a42d0ee9db058388
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-11-06 22:59:39 +0200

    api routes / frontend routes / +layout

---
+++ src/components/Nav.svelte
@@ -0,0 +1,4 @@
+<ul class="flex gap-5">
+	<li><a href="/">Home</a></li>
+	<li><a href="/posts">Posts</a></li>
+</ul>
---
+++ src/routes/api/+server.js
@@ -0,0 +1,28 @@
+import { json } from '@sveltejs/kit';
+
+const data = [{ message: 'Hello World!' }];
+
+export const GET = () => {
+	return json(data);
+};
+
+export const POST = async (req) => {
+	const reqBody = await req.request.json();
+	data.push(reqBody);
+
+	return json(data);
+};
+
+export const PUT = async (req) => {
+	const message = await req.request.json();
+	const id = Number(req.url.searchParams.get('id')) || 0;
+	data[id] = message;
+
+	return json(data);
+};
+
+export const DELETE = async (req) => {
+	const id = Number(req.url.searchParams.get('id')) || 0;
+	data.splice(id, 1);
+	return json(data);
+};
